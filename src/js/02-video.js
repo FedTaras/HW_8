35ec35bca1b75e03458aeadaa566f928f9347b75
FedTaras <113994712+FedTaras@users.noreply.github.com>
2024-03-06 21:23:48 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    seconds = 0;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+    }
+  });
+
+const onPlay = function (evt) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+};
+
+player.on('timeupdate', _throttle(onPlay, 1000));
