88a233984431648759c4add59d16690228a7d59f
FedTaras <113994712+FedTaras@users.noreply.github.com>
2024-03-06 18:02:08 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+console.log(galleryItems);
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+galleryList.style.listStyle = 'none';
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"/>
+      </a>
+    </li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox');
